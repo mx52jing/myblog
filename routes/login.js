11951ae466d7ad16e61fec87d1a5deaa476e96fd
孟轩 <mengxuan@mobase.cn>
2017-08-15 18:08:12 +0800

    页面功能完善

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const router = express.Router();
-
+const sha1 = require('sha1');
+const UserModel = require('../models/user');
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 // GET /login 登录页
 router.get('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+  res.render('login');
 });
 
 // POST /login 用户登录
 router.post('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+  var name = req.fields.name;
+  var password = req.fields.password;
+  UserModel.getUserByName(name)
+  .then((user) => {
+    if(!user) {
+      req.flash('error', '用户不存在');
+      return res.redirect('back');
+    }
+    // 检查密码是否匹配
+    if(sha1(password) !== user.password){
+      req.flash('error', '用户名或密码错误');
+      return res.redirect('back');
+    }
+    req.flash('success', '登录成功');
+    // 用户信息写入 session
+    delete user.password;
+    req.session.user = user;
+    // 跳转到主页
+    res.redirect('/posts');
+  })
+  .catch(next)
 });
 
 module.exports = router;
